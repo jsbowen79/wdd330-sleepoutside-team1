bec655834a0c5a9bfdc08acc60bbbe7c17668f9a
jsbowen79 <jsbowen79@outlook.com>
2026-03-04 10:16:46 -0700

    Fixed shopping Cart Function

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,20 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  //If  local storage exists add product to a list of items and store the list in local storage using JSON.
+
+  let storedData = getLocalStorage("so-cart");
+  if (storedData) {
+    storedData.push(product);
+    setLocalStorage("so-cart", storedData);
+    //If local storage does not exist, create a list, add product to the list and save the list to local storage using JSON.
+  } else {
+    let cartList = [product];
+    setLocalStorage("so-cart", cartList);
+  }
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
