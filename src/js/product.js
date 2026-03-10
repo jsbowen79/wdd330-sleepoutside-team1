aee0f43be688da86607a110512d3afcd44d37839
oedesign <oluwaseyielujoba15@gmail.com>
2026-03-10 14:59:30 +0100

    I fixed the W01 Add item to cart bug and submitted my pull request.

--- src/js/product.js
+++ src/js/product.js
@@ -2,9 +2,9 @@ import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
-let productsArray = getLocalStorage("so-cart") || [];
 
 function addProductToCart(product) {
+  let productsArray = getLocalStorage("so-cart") || [];
   productsArray.push(product);
   setLocalStorage("so-cart", productsArray);
 }
