471cda090e0dd9e0206565efd1e167af04ee43cf
SofiaAS784 <ssuarez2@byupathway.edu>
2026-03-21 12:51:07 -0300

    Code fixed and working

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -2,7 +2,7 @@ import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
     return `<li class="product-card">
-    <a href="product_pages/?id=${product.Id}">
+    <a href="/product_pages/?product=${product.Id}">
       <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
       <h2 class="card__brand">${product.Brand.Name}</h2>
       <h3 class="card__name">${product.NameWithoutBrand}</h3>
@@ -23,6 +23,7 @@ export default class ProductList {
     async init() {
         const list = await this.dataSource.getData(this.category);
         this.renderList(list);
-        document.querySelector('.title').textContent = this.category;
+        const categoryTitle = this.category.charAt(0).toUpperCase() + this.category.slice(1);
+        document.querySelector('.title').textContent = categoryTitle;
     }
 }
